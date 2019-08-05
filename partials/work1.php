cdb361a3196522c18f9bd25239f9ce6d18361445
ham8821 <ham38538821@gmail.com>
2019-08-05 23:23:35 +1200

    projects responsive in progress

---
+++ partials/work1.php
@@ -0,0 +1,11 @@
+<div class="wrap">
+  <button class="js-open btn-open is-active">Show modal</button>
+  <div class="modal js-modal">
+    <div class="modal-image">
+      <svg viewBox="0 0 32 32" style="fill:#48DB71"><path d="M1 14 L5 10 L13 18 L27 4 L31 8 L13 26 z"></path></svg>
+    </div>
+    <h1>Nice job!</h1>
+    <p>To dismiss click the button below</p>
+    <button class="js-close">Dismiss</button>
+  </div>
+</div>
--- works.php
+++ works.php
@@ -10,12 +10,16 @@
     <?php
     include_once("partials/head.php");
     include_once("css/style.php");
+    // include_once("partials/work1.php");
     ?>
     <script>
-        $( document ).ready(function() {
-        $('body').show();
-         $('.projecthead').animate({'opacity':'1','marginLeft': '0.1in'},1500);
-    });
+        $(document).ready(function() {
+            $('body').show();
+            $('.projecthead').animate({
+                'opacity': '1',
+                'marginBottom': '0.1in'
+            }, 1500);
+        });
     </script>
 </head>
 
@@ -27,19 +31,85 @@
     <!-- /.navbar -->
     <!--head part  -->
     <section>
-    <div class="container-fluid">
+
         <div class="row">
             <div class="col-12 headpart">
                 <div class="projecthead">
-                    <span class="projectmain">PROJECT</span>
+                    <span class="projectmain">PROJECTS</span>
                     <span><button class="morebtn"><a href="https://github.com/ham8821"> More</a></button></span>
                 </div>
             </div>
             <div class="col-12 workpage">
+                <div class="workwrapper contatiner-fluid">
 
+                    <div class="card">
+
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/liquorlibrary.png" alt="">
+                        </div>
+                        <div class="col-12 descard">
+                            <div class="projectdescription">
+                                <p class="projectname">Liquor Library</p>
+                                <p class="projectarea">Web development <span>/Mar 2019-present</span></p>
+                                <p class="projecttech">PHP, MySQL,HTML, CSS, JavaScript,<br> JQuery, Ajax,
+                                    bootstrap 4 ,git, azure DevOps, AWS</p>
+                                    <div class="w-like">
+                      <span class="ion-ios-plus-outline"><img style="width:15px; height:15px; color: rgba(242, 201, 21, 1);" src="images/add-circular-button-outline-with-plus-sign.svg" alt=""></span>
+                    </div>
+                                <!-- <p class="projectcontext">Solving business problem in targeting online customers by
+                                    delivering solid and desirable online web service.
+                                    knowledge of software development and programming with
+                                    understanding of initial flow, covering from database designing
+                                    to testing to deliver the product.</p> -->
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/liquorlibrary.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/thrifters.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/thrifters.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/itonline.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/itonline.png" alt="">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/carrental.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="col-12 imgcard">
+                            <img class="workimg img-responsive" src="images/carrental.png" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+
+
     </section>
     <!-- head part ends -->
     <?php
