b14952c9a5bc4b6a4df538399e6615655c4d2491
ham8821 <ham38538821@gmail.com>
2019-08-05 13:50:42 +1200

    skill button added

--- index.php
+++ index.php
@@ -58,22 +58,39 @@
 
         <div class="container-fluid full-screen">
             <div class="row">
-                <div class="introscreen col-xs-12 col-md-12" style="height: 100vh;">
+                <div class="introscreen col-xs-12 col-md-12">
                     <div id="introductionbox">
 
-                        <p class="jobtitle" style=" margin-top: 10%;">Front-end developer </p>
-                        <p class="jobtitle">&nbsp &nbsp & Back-end developer</p>
-                        <br>
-                        <br>
+                        <p class="jobtitle" style=" margin-top: 50px;">Front-end developer </p>
+                        <p class="jobtitle" style=" margin-bottom: 20px;">&nbsp &nbsp & Back-end developer</p>
+
                         <div class="collapse navbar-collapse" id="extrades">
                             <p class="intro">Having completed bachelor degree in Business Administration and Graduate Diploma in Information Technology this year, I've been always pursuing learning new things day by day. <br> What I always make sure in daily basis is whether I have learnt new things apart from school study scope, having personal projects aside all the time. At the moment, I am working for this liquor speacialist company, building their official website as a contract developer. If you are interested, you can have a look some of my works here.</p>
                         </div>
-                        <br><br>
+                        <br>
                         <p class="intro"> I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p>
-                        <button type="button" style="background-color:black; color: white; border-radius:5px; float:none; font-size:1.2em; border:none;"  class="navbar-toggle readmore" data-toggle="collapse" data-target="#extrades">
+                        <button type="button" style="background-color:black; color: white; border-radius:5px; float:none; font-size:1em; border:none;" class="navbar-toggle readmore" data-toggle="collapse" data-target="#extrades">
                             <span>READ MORE</span>
                         </button>
 
+                        <div class="row">
+                            <div class="skillbtn">
+                                <div class="buttons">
+                                    <button class="btn-hover color-8">HTML5</button>
+                                    <button class="btn-hover color-8">CSS</button>
+                                    <button class="btn-hover color-8">Javascript</button>
+                                    <button class="btn-hover color-8">bootstrap</button>
+                                    <button class="btn-hover color-8">Jq</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                    <button class="btn-hover color-8">BUTTON</button>
+                                </div>
+            
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -146,3 +163,96 @@
 </body>
 
 </html>
+<style>
+    
+
+/* button design */
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.buttons {
+  margin: 10px auto;
+  width: 80%;
+  text-align: center;
+}
+
+.btn-hover {
+  width: 120px;
+  font-size: 14px;
+  font-weight: 600;
+  color: #fff;
+  cursor: pointer;
+  margin: 20px;
+  height: 40px;
+  text-align:center;
+  border: none;
+  background-size: 300% 100%;
+
+  border-radius: 50px;
+  moz-transition: all .4s ease-in-out;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
+}
+
+.btn-hover:hover {
+  background-position: 100% 0;
+  moz-transition: all .4s ease-in-out;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
+}
+
+.btn-hover:focus {
+  outline: none;
+}
+
+.btn-hover.color-1 {
+  background-image: linear-gradient(to right, #25aae1, #40e495, #30dd8a, #2bb673);
+  box-shadow: 0 4px 15px 0 rgba(49, 196, 190, 0.75);
+}
+.btn-hover.color-2 {
+  background-image: linear-gradient(to right, #f5ce62, #e43603, #fa7199, #e85a19);
+  box-shadow: 0 4px 15px 0 rgba(229, 66, 10, 0.75);
+}
+.btn-hover.color-3 {
+  background-image: linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7);
+  box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
+}
+.btn-hover.color-4 {
+  background-image: linear-gradient(to right, #fc6076, #ff9a44, #ef9d43, #e75516);
+  box-shadow: 0 4px 15px 0 rgba(252, 104, 110, 0.75);
+}
+.btn-hover.color-5 {
+  background-image: linear-gradient(to right, #0ba360, #3cba92, #30dd8a, #2bb673);
+  box-shadow: 0 4px 15px 0 rgba(23, 168, 108, 0.75);
+}
+.btn-hover.color-6 {
+  background-image: linear-gradient(to right, #009245, #FCEE21, #00A8C5, #D9E021);
+  box-shadow: 0 4px 15px 0 rgba(83, 176, 57, 0.75);
+}
+.btn-hover.color-7 {
+  background-image: linear-gradient(to right, #6253e1, #852D91, #A3A1FF, #F24645);
+  box-shadow: 0 4px 15px 0 rgba(126, 52, 161, 0.75);
+}
+.btn-hover.color-8 {
+  background-image: linear-gradient(to right, #29323c, #485563, #2b5876, #4e4376);
+  box-shadow: 0 4px 15px 0 rgba(45, 54, 65, 0.75);
+}
+.btn-hover.color-9 {
+  background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+  box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+.btn-hover.color-10 {
+      background-image: linear-gradient(to right, #ed6ea0, #ec8c69, #f7186a , #FBB03B);
+  box-shadow: 0 4px 15px 0 rgba(236, 116, 149, 0.75);
+}
+.btn-hover.color-11 {
+     background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+}
+
+
+    </style>
