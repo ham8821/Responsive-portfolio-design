69d3de0e3858da88dd899f9b0f530ac55f1a206c
ham8821 <ham38538821@gmail.com>
2019-08-19 00:23:57 +1200

    Revert "df"
    This reverts commit 97e943e9231c85be6d08b0937d6c9fdc0a67f627.

---
+++ projects.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/secondcss.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Roboto|Oswald|Playfair+Display" rel="stylesheet">
+    <script type="text/javascript" src="main.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+
+    <title>Projects</title>
+</head>
+<body class="fadeIn">
+    
+<!-- ----------------- -->
+
+<div class="wrapper">
+        <div class="site-header-main">
+            <div class="site-branding">
+
+<div class="site-identity">
+            <p class="site-title">My projects</p>
+
+            <p class="site-description">Just another WordPress site</p>
+    </div>
+</div><!-- .site-branding -->
+
+            <div id="site-header-menu" class="site-header-menu">
+<div id="primary-menu-wrapper" class="menu-wrapper">
+
+<div class="header-overlay"></div>
+
+<div class="menu-toggle-wrapper">
+    <button type="button" id="menu-toggle" class="menu-toggle" aria-controls="top-menu" aria-expanded="false" data-toggle="collapse" data-target="#site-navigation">
+        <div class="menu-bars">
+              <div class="bars bar1"></div>
+              <div class="bars bar2"></div>
+              <div class="bars bar3"></div>
+          </div>
+        <span class="menu-label">Menu</span>
+    </button>
+</div><!-- .menu-toggle-wrapper -->
+<br>
+<div class="menu-inside-wrapper">
+
+        
+<nav id="site-navigation" class="main-navigation default-page-menu" role="navigation" aria-label="Primary Menu" aria-expanded="false">
+<div class="primary-menu-container"><ul id="menu-primary-items" class="menu nav-menu"><li class="page_item page-item-2"><a href="#">Sample Page</a></li></ul></div>
+<div class="primary-menu-container"><ul id="menu-primary-items" class="menu nav-menu"><li class="page_item page-item-2"><a href="#">Sample Page</a></li></ul></div>
+<div class="primary-menu-container"><ul id="menu-primary-items" class="menu nav-menu"><li class="page_item page-item-2"><a href="#">Sample Page</a></li></ul></div>
+<div class="primary-menu-container"><ul id="menu-primary-items" class="menu nav-menu"><li class="page_item page-item-2"><a href="#">Sample Page</a></li></ul></div>
+<div class="primary-menu-container"><ul id="menu-primary-items" class="menu nav-menu"><li class="page_item page-item-2"><a href="#">Sample Page</a></li></ul></div>
+
+</nav><!-- .main-navigation -->
+</div><!-- .menu-inside-wrapper -->
+</div><!-- #primary-menu-wrapper.menu-wrapper -->
+
+</div><!-- .site-header-menu -->
+
+
+        </div> <!-- .site-header-main -->
+
+        <!-- <div id="header-navigation-area">
+                            </div> --> <!-- #header-navigation-area -->
+
+    </div>
+<!--  -->
+<section>
+    <div class="container-fluid">
+        <div class="row" style="padding-left: 300px; margin-top:50px;">
+           
+            <!-- <div class="projects col-sm-6 col-md-3 col-lg-3"><img src="images/Thrifters.png" class="img-fluid" alt=""></div> -->
+            <div class="projects col-sm-6 col-md-3 col-lg-3"></div>
+            <div class="projects col-sm-6 col-md-3 col-lg-3"></div>
+            <div class="projects col-sm-6 col-md-3 col-lg-3"></div>
+            <div class="projects col-sm-6 col-md-3 col-lg-3"></div>
+            <div class="projects col-sm-6 col-md-3 col-lg-3"></div>
+        </div>
+
+    </div>
+</section>
+    <!-- <div class="container">
+        <div class="row">
+            <div class="Wrapper2 col-12">
+                Copyright © 2019 ham8821's. All Rights Reserved.  | 	</div> 
+        </div> 
+    </div> -->
+</body>
+</html>
