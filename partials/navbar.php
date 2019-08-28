8ace626212fe98b04353e27304e68d03548b78d4
ham8821 <ham38538821@gmail.com>
2019-08-28 19:16:52 +1200

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,3 +1,13 @@
+<?php 
+// Program to display complete URL 
+  
+$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] 
+                === 'on' ? "https" : "http") . "://" . 
+          $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']; 
+  
+// Display the complete URL 
+echo $link; 
+?> 
 <nav class="navbar-fixed-top navbar-default opaque-navbar">
         <div class="container-fluid">
             <div id="navbar" class="navbar-collapse full-screen">
@@ -13,11 +23,29 @@
 
 
                 <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
+                    <?php
+                    if($link =='https://emmaham-portfolio.herokuapp.com/works.php' or $link =='http://localhost/portfolio/Responsive-portfolio-design/works.php'){
+                        echo'  <ul class="nav navbar-nav navbar-right">
                         <li class="active navlist"><a href="index.php">Home</a></li>
-                        <li class="navlist"><a href= "#introduction">About</a></li>
+                        <li class="navlist"><a href= "index.php #introduction">About</a></li>
                         <li class="navlist"><a href="works.php">Projects</a></li>
-                        <li class="navlist"><a href="#Contact">Contact</a></li>
+                        <li class="navlist"><a href="index.php #Contact">Contact</a></li>';   
+                    }
+                    else if ($link =='https://emmaham-portfolio.herokuapp.com/index.php' or $link =='http://localhost/portfolio/Responsive-portfolio-design/index.php'){
+                        echo'  <ul class="nav navbar-nav navbar-right">
+                              <li class="active navlist"><a href="index.php">Home</a></li>
+                              <li class="navlist"><a href= "#introduction">About</a></li>
+                              <li class="navlist"><a href="works.php">Projects</a></li>
+                              <li class="navlist"><a href="#Contact">Contact</a></li>';
+                    }
+                   else{
+                    echo'  <ul class="nav navbar-nav navbar-right">
+                    <li class="active navlist"><a href="index.php">Home</a></li>
+                    <li class="navlist"><a href= "#introduction">About</a></li>
+                    <li class="navlist"><a href="works.php">Projects</a></li>
+                    <li class="navlist"><a href="#Contact">Contact</a></li>';
+                   }
+                        ?>
                     </ul>
                 </div>
             </div>
