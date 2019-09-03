9dd769c34161766df3a324c74a8a5b05f9a48831
ham8821 <ham38538821@gmail.com>
2019-09-03 15:59:02 +1200

    up

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ elseif(!ob_start("ob_gzhandler")) {
 
         <div class="container-fluid full-screen" id="parentjob">
             <div class="row">
-                <div class="introscreen col-xs-12 col-md-12">
+                <div class="introscreen col-xs-12 col-md-12 col-lg-12 col-xl-12">
                     <div id="introductionbox" id="childjob">
 
                         <p class="jobtitle hideme" style=" margin-top: 50px; margin-right:80px;">Front-end developer </p>
@@ -95,7 +95,7 @@ elseif(!ob_start("ob_gzhandler")) {
         <div class="container-fluid full-screen">
             <div class="row">
                 <div class="workscreen col-xs-12 col-md-12 col-lg-12">
-                    <!-- <br><br><br><br><br><br><br> <br><br><br><br><br><br><br> -->
+                    <br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br>
                     <p class="recentwork hideme">My recent Projects</p>
                     <div class="iconbox hideme"><a href="#"> <span class="iconify" data-icon="jam:chevron-circle-right" data-inline="false" onclick="transitionToPage('works.php')"></span></a></div>
                 </div>
